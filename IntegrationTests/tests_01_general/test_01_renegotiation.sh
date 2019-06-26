06eea9ac37d8f04edcbb32531c7f2851b0f30c44
Cory Benfield <lukasa@apple.com>
2019-06-26 21:08:31 +0100

    Support renegotiation-based client auth. (#111)
    Motivation:
    
    While TLS renegotiation is in general a bad idea, it is widely used for
    delayed TLS-based client authentication. We are able to support this
    use-case, and so we should enable it.
    
    As with BoringSSL, we'd like to continue to default to not supporting
    this mode of operation. However, users are free to enable support if
    they so choose.
    
    Modifications:
    
    - Added flags for configuring renegotiation.
    - Added integration test harness.
    - Added renegotiation integration test with s_server.
    - Added renegotiation and client cert support to sample client.
    
    Result:
    
    Users will be able to perform TLS renegotiation.

---
+++ IntegrationTests/tests_01_general/test_01_renegotiation.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftNIO open source project
+##
+## Copyright (c) 2019 Apple Inc. and the SwiftNIO project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+source defines.sh
+
+swift build
+
+# Generate a self-signed certificate.
+cat << EOF > "$tmp/openssl.cnf"
+[ req ]
+distinguished_name = subject
+req_extensions = req_ext
+x509_extensions = x509_ext
+
+[ subject ]
+countryName         = Country Name (2 letter code)
+countryName_default     = US
+
+stateOrProvinceName     = State or Province Name (full name)
+stateOrProvinceName_default = NY
+
+localityName            = Locality Name (eg, city)
+localityName_default        = New York
+
+organizationName         = Organization Name (eg, company)
+organizationName_default    = Example, LLC
+
+[ req_ext ]
+
+basicConstraints = CA:FALSE
+
+[ x509_ext ]
+subjectKeyIdentifier = hash
+subjectAltName = @alternate_names
+
+[ alternate_names ]
+DNS.1 = localhost
+EOF
+
+
+openssl req -new -newkey rsa:4096 -days 365 -nodes -config "$tmp/openssl.cnf" -x509 \
+    -subj "/C=US/ST=NJ/L=Wall/O=NIO/CN=localhost" \
+    -keyout "$tmp/key.pem" -out "$tmp/cert.pem"
+
+expect -c "
+          spawn openssl s_server -cert \"$tmp/cert.pem\" -key \"$tmp/key.pem\"
+          set serverspawn \$spawn_id
+          expect {
+              \"ACCEPT\" {
+              }
+              timeout {
+                  exit 1
+              }
+          }
+
+          spawn $(client_path) http://localhost:4433/get \"$tmp/cert.pem\" \"$tmp/key.pem\" \"$tmp/cert.pem\"
+          set spawn_id \$serverspawn
+
+          expect {
+              \"close\\r\\r\" {
+              }
+              timeout {
+                  exit 2
+              }
+          }
+          send  \"R\r\"
+          expect {
+              \"Read BLOCK\\r\" {
+              }
+              timeout {
+                  exit 3
+              }
+          }
+          "
+
