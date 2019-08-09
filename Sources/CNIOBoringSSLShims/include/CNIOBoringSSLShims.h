b112dd67c4b31e32b946e7fe5c8867dea1cf6383
George Barnett <gbarnett@apple.com>
2019-08-09 10:18:45 +0100

    Add comment about CNIOBoringSSLShims import

--- Sources/CNIOBoringSSLShims/include/CNIOBoringSSLShims.h
+++ Sources/CNIOBoringSSLShims/include/CNIOBoringSSLShims.h
@@ -14,6 +14,8 @@
 #ifndef C_NIO_BORINGSSL_SHIMS_H
 #define C_NIO_BORINGSSL_SHIMS_H
 
+// This is for instances when `swift package generate-xcodeproj` is used as CNIOBoringSSL
+// is treated as a framework and requires the framework's name as a prefix.
 #if __has_include(<CNIOBoringSSL/CNIOBoringSSL.h>)
 #include <CNIOBoringSSL/CNIOBoringSSL.h>
 #else
