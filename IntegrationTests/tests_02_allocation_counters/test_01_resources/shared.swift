f22bf05ea309a58fb4be6ef6fd2f1c6bbbc09239
Cory Benfield <lukasa@apple.com>
2019-11-27 11:51:12 +0000

    Add allocation counter tests.
    Motivation:
    
    To do proper performance work it's necessary to have insight into what
    programs are actually doing. One of the two major axes the SwiftNIO
    project uses to measure is heap allocations. Currently swift-nio-ssl has
    no introspection of heap allocations, so doing informed performance work
    is very hard.
    
    Modifications:
    
    - Added the allocation counter test framework.
    - Added two allocation counter tests.
    
    Result:
    
    Better idea of what's going on in heap allocations.

---
+++ IntegrationTests/tests_02_allocation_counters/test_01_resources/shared.swift
@@ -0,0 +1,99 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2019 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+import Foundation
+import NIO
+import NIOSSL
+
+class BackToBackEmbeddedChannel {
+    private(set) var client: EmbeddedChannel
+    private(set) var server: EmbeddedChannel
+    private var loop: EmbeddedEventLoop
+
+
+    init() {
+        self.loop = EmbeddedEventLoop()
+        self.client = EmbeddedChannel(loop: self.loop)
+        self.server = EmbeddedChannel(loop: self.loop)
+    }
+
+    func run() {
+        self.loop.run()
+    }
+
+    func interactInMemory() throws {
+        var workToDo = true
+
+        while workToDo {
+            workToDo = false
+            
+            self.loop.run()
+            let clientDatum = try self.client.readOutbound(as: IOData.self)
+            let serverDatum = try self.server.readOutbound(as: IOData.self)
+
+            if let clientMsg = clientDatum {
+                try self.server.writeInbound(clientMsg)
+                workToDo = true
+            }
+
+            if let serverMsg = serverDatum {
+                try self.client.writeInbound(serverMsg)
+                workToDo = true
+            }
+        }
+    }
+}
+
+
+extension BackToBackEmbeddedChannel {
+    enum Error: Swift.Error {
+        case nonCleanExit
+    }
+}
+
+
+extension NIOSSLCertificate {
+    static func forTesting() throws -> NIOSSLCertificate {
+        return try .init(bytes: certificatePemBytes, format: .pem)
+    }
+}
+
+
+extension NIOSSLPrivateKey {
+    static func forTesting() throws -> NIOSSLPrivateKey {
+        return try .init(bytes: keyPemBytes, format: .pem)
+    }
+}
+
+
+fileprivate let certificatePemBytes = Array("""
+-----BEGIN CERTIFICATE-----
+MIIBTzCB9qADAgECAhQkvv72Je/v+B/cgJ53f84O82z6WTAKBggqhkjOPQQDAjAU
+MRIwEAYDVQQDDAlsb2NhbGhvc3QwHhcNMTkxMTI3MTAxMjMwWhcNMjkxMTI0MTAx
+MjMwWjAUMRIwEAYDVQQDDAlsb2NhbGhvc3QwWTATBgcqhkjOPQIBBggqhkjOPQMB
+BwNCAAShtZ9TRt7I+7Y0o99XUkrgSYmUmpr4K8CB0IkTCX6b1tXp3Xqs1V5BckTd
+qrls+zsm3AfeiNBb9EDdxiX9DdzuoyYwJDAUBgNVHREEDTALgglsb2NhbGhvc3Qw
+DAYDVR0TAQH/BAIwADAKBggqhkjOPQQDAgNIADBFAiAKxYON+YTnIHNR0R6SLP8R
+R7hjsjV5NDs18XLoeRnA1gIhANwyggmE6NQW/r9l59fexj/ZrjaS3jYOTNCfC1Lo
+5NgJ
+-----END CERTIFICATE-----
+""".utf8)
+
+
+fileprivate let keyPemBytes = Array("""
+-----BEGIN PRIVATE KEY-----
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgCn182hBmYVMAiNPO
++7w05F40SlAqqxgBEYJZOeK47aihRANCAAShtZ9TRt7I+7Y0o99XUkrgSYmUmpr4
+K8CB0IkTCX6b1tXp3Xqs1V5BckTdqrls+zsm3AfeiNBb9EDdxiX9Ddzu
+-----END PRIVATE KEY-----
+""".utf8)
