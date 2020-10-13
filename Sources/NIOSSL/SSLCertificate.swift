878da720cc0a37697363673db4e9a3c730b7391e
David Evans <d.evans@apple.com>
2020-10-13 16:17:54 +0100

    Update Sources/NIOSSL/SSLCertificate.swift
    Co-authored-by: Cory Benfield <lukasa@apple.com>

--- Sources/NIOSSL/SSLCertificate.swift
+++ Sources/NIOSSL/SSLCertificate.swift
@@ -490,7 +490,7 @@ extension NIOSSLCertificate.IPAddress: CustomStringConvertible {
         var address = address
         var dest: [CChar] = Array(repeating: 0, count: NIOSSLCertificate.IPAddress.ipv4AddressLength)
         dest.withUnsafeMutableBufferPointer { pointer in
-            let result = inet_ntop(AF_INET6, &address, pointer.baseAddress!, socklen_t(NIOSSLCertificate.IPAddress.ipv6AddressLength))
+            let result = inet_ntop(AF_INET6, &address, pointer.baseAddress!, socklen_t(pointer.count))
             precondition(result != nil, "The IP address was invalid. This should never happen as we're within the IP address struct.")
         }
         return String(cString: &dest)
