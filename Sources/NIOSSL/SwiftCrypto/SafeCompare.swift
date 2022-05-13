fbaa52b24d70bc5d111d2044e984a079ca8a8b73
Matt Eaton <agnosticdev@gmail.com>
2022-05-13 01:05:56 -0700

    Port SecureBytes from Swift Crypto to use with TLS-PSK (#366)
    * Port SecureBytes from Swift Crypto to use with TLS-PSK
    
    Motivation:
    
    Porting SecureBytes to NIOSSLSecureBytes for using in (Add TLS-PSK Support #360).
    
    Modifications:
    
    Addition and modification of SecureBytes to work within SwiftNIO SSL.
    Added additional test cases.
    
    Result:
    
    NIOSSLSecureBytes will be ported for usage in a later PR with TLS-PSK.
    
    * secure-bytes-transfer: Added LinuxMain tests
    
    * secure-bytes-transfer: Removed case from NIOSSLError
    
    * secure-bytes-transfer: Addressed initial feedback
    
    * secure-bytes-transfer: Removed NIOSSLSecureBytesError from SSLErrors
    
    * secure-bytes-transfer: Remove public access from NIOSSLSecureBytes
    
    * Remove remaining public declaration.
    
    Co-authored-by: meaton2 <meaton3@apple.com>
    Co-authored-by: Cory Benfield <lukasa@apple.com>

---
+++ Sources/NIOSSL/SwiftCrypto/SafeCompare.swift
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2022 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// A straightforward constant-time comparison function for any two collections of bytes.
+@inlinable
+internal func constantTimeCompare<LHS: Collection, RHS: Collection>(_ lhs: LHS, _ rhs: RHS) -> Bool where LHS.Element == UInt8, RHS.Element == UInt8 {
+    guard lhs.count == rhs.count else {
+        return false
+    }
+
+    return zip(lhs, rhs).reduce(into: 0) { $0 |= $1.0 ^ $1.1 } == 0
+}
