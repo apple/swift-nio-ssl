f3407a5ea0c125c751fd7072a140f5ba0f980362
meatonAdmin <meatonadmin@meatonAdmins-MacBook-Pro.local>
2022-06-14 08:19:45 -0700

    issue-230 5.7 fix

--- Sources/NIOSSL/SSLContext.swift
+++ Sources/NIOSSL/SSLContext.swift
@@ -296,13 +296,14 @@ public final class NIOSSLContext {
             if let clientSideCallback = pskCallback.clientCallback {
                 let defaultClientPSKInfo = clientSideCallback(nil)
                 let indentityBytes = defaultClientPSKInfo.identity.backing.withUnsafeBytes{  Array($0) }
-                CNIOBoringSSL_SSL_CTX_use_psk_identity_hint(context, String(cString:indentityBytes))
+                
+                CNIOBoringSSL_SSL_CTX_use_psk_identity_hint(context, String(bytes: indentityBytes, encoding: .utf8))
                 CNIOBoringSSL_SSL_CTX_set_psk_client_callback(context, { clientPSKCallback(ssl: $0, hint: $1, identity: $2, max_identity_len: $3, psk: $4, max_psk_len: $5 ) } )
             // Ohterwise see if the server side callback is present.
             } else if let serverSideCallback = pskCallback.serverCallback {
                 let defaultServerPSKInfo = serverSideCallback(nil, nil) // nil, nil being passed in as a way to pass back default data.
                 let indentityBytes = defaultServerPSKInfo.identity.backing.withUnsafeBytes{  Array($0) }
-                CNIOBoringSSL_SSL_CTX_use_psk_identity_hint(context, String(cString:indentityBytes))
+                CNIOBoringSSL_SSL_CTX_use_psk_identity_hint(context, String(bytes: indentityBytes, encoding: .utf8))
                 CNIOBoringSSL_SSL_CTX_set_psk_server_callback(context, { serverPSKCallback(ssl: $0, identity: $1, psk: $2, max_psk_len: $3 )})
             }
         }
