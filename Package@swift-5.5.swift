86cdb415ef8f904bd57409a8ec8d8779ecb4736b
Cory Benfield <lukasa@apple.com>
2022-09-27 13:46:53 +0100

    Replace Lock with NIOLock
    Motivation:
    
    Lock has been deprecated in favour of NIOLock. Warnings aren't great.
    
    Modifications:
    
    - Replace Lock with NIOLock
    - Update the minimum required NIO version to 2.42.0.
    
    Result:
    
    Everything builds cleanly

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -36,7 +36,7 @@ import class Foundation.ProcessInfo
 func generateDependencies() -> [Package.Dependency] {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         return [
-            .package(url: "https://github.com/apple/swift-nio.git", from: "2.32.0"),
+            .package(url: "https://github.com/apple/swift-nio.git", from: "2.42.0"),
         ]
     } else {
         return [
