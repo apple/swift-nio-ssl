4fabf57d9b15cc6cf6d591e29ddbd4660b53dc9b
Rick Newton-Rogers <rnro@apple.com>
2024-10-01 08:46:26 +0100

    Update expired test certificate
    Motivation:
    
    An expired test certificate was causing tests to fail.
    
    Modifications:
    
    Updated the test certificate.
    
    Result:
    
    Tests should pass.

--- Tests/NIOSSLTests/SecurityFrameworkVerificationTests.swift
+++ Tests/NIOSSLTests/SecurityFrameworkVerificationTests.swift
@@ -152,46 +152,46 @@ extension SecurityFrameworkVerificationTests {
     static let appleComCertChain: [SecCertificate] = {
         // All certs here are PEM format, with the leading/trailing lines stripped.
         let leaf = """
-            MIIHfDCCBmSgAwIBAgIQATyyJFiUVZ8X5Gj1MTLNCDANBgkqhkiG9w0BAQsFADBR
+            MIIHejCCBmKgAwIBAgIQFEfPshq/qaj2Zf8Y/vVgUjANBgkqhkiG9w0BAQsFADBR
             MQswCQYDVQQGEwJVUzETMBEGA1UEChMKQXBwbGUgSW5jLjEtMCsGA1UEAxMkQXBw
-            bGUgUHVibGljIEVWIFNlcnZlciBSU0EgQ0EgMiAtIEcxMB4XDTI0MDYyMDE5MTI0
-            NVoXDTI0MDkxODE5MjI0NVowgccxHTAbBgNVBA8MFFByaXZhdGUgT3JnYW5pemF0
+            bGUgUHVibGljIEVWIFNlcnZlciBSU0EgQ0EgMiAtIEcxMB4XDTI0MDgyMzE3MzAx
+            MVoXDTI0MTEyMTE3NDAxMVowgccxHTAbBgNVBA8MFFByaXZhdGUgT3JnYW5pemF0
             aW9uMRMwEQYLKwYBBAGCNzwCAQMTAlVTMRswGQYLKwYBBAGCNzwCAQIMCkNhbGlm
             b3JuaWExETAPBgNVBAUTCEMwODA2NTkyMQswCQYDVQQGEwJVUzETMBEGA1UECAwK
             Q2FsaWZvcm5pYTESMBAGA1UEBwwJQ3VwZXJ0aW5vMRMwEQYDVQQKDApBcHBsZSBJ
             bmMuMRYwFAYDVQQDDA13d3cuYXBwbGUuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOC
-            AQ8AMIIBCgKCAQEAoZMSzyh9Kc89VO0rbdBwczEYq/vVCFstY4wfqwM5Ujw22hER
-            Ao68GIkowTmNEoPCExbYqZPOBkTU7PD8VDV/7Dz8vvvBfPc2eWaRbuabOjATNQIx
-            bi5k+mbKTbr1+lg33FEKzeFonSYkL6kqD6Nf54xRPoXa9kNYQeuwtUZtg4xMxNh8
-            46LnbXt0HdgIHJ60TgLxnhYQtFocltz+kJUlzQmPf4fS77xtc35caFk79kMw9zxd
-            R6G9l9u/TR3PsgI9+zjS23/K4z0/quW/uwr5dILIG1F5HQ6xr0PreskMxd087mVy
-            2q0tfkWN4+a7krs1xP/bQHwjVgz9mgI5E2GVJQIDAQABo4ID1zCCA9MwDAYDVR0T
+            AQ8AMIIBCgKCAQEAyKICisEcp5ruWEmbEDxBjb/vbyN+ZAXpuSBXOzZbntL5ZN75
+            Wq+ELBDBCNHs5NJ01x4rw7rEF36xyJ9dNBvjyAb5yNu7w68WRmoEIlTIFC1DYSHs
+            xNWU/7ot305j/p12v6yqSco7pCUfi1JrYeqd8oBXYrJauzUyjP96dT9L+7l3wKm6
+            jbS0NbG62C6W4bcHxdq5UPTIVJDjqibZekMfYnM/iOtctx/SdCvHOdBzyl1Ks0tZ
+            ItFUxUHfpqXBKjnFMUg5g+rqLXc7UUSSr6Z5Ro6Cju+wPWsUvxZ0QAkvvBmlIY21
+            vDUsUQv7ZsaiJFY4WO6Y+zu3KZVONu4N8mPQCQIDAQABo4ID1TCCA9EwDAYDVR0T
             AQH/BAIwADAfBgNVHSMEGDAWgBRQVatDoa+pSCtawaKHiQTkeg7K2jB6BggrBgEF
             BQcBAQRuMGwwMgYIKwYBBQUHMAKGJmh0dHA6Ly9jZXJ0cy5hcHBsZS5jb20vYXBl
             dnNyc2EyZzEuZGVyMDYGCCsGAQUFBzABhipodHRwOi8vb2NzcC5hcHBsZS5jb20v
-            b2NzcDAzLWFwZXZzcnNhMmcxMDEwPAYDVR0RBDUwM4IQaW1hZ2VzLmFwcGxlLmNv
-            bYIQd3d3LmFwcGxlLmNvbS5jboINd3d3LmFwcGxlLmNvbTBgBgNVHSAEWTBXMEgG
+            b2NzcDAzLWFwZXZzcnNhMmcxMDEwPAYDVR0RBDUwM4INd3d3LmFwcGxlLmNvbYIQ
+            aW1hZ2VzLmFwcGxlLmNvbYIQd3d3LmFwcGxlLmNvbS5jbjBgBgNVHSAEWTBXMEgG
             BWeBDAEBMD8wPQYIKwYBBQUHAgEWMWh0dHBzOi8vd3d3LmFwcGxlLmNvbS9jZXJ0
             aWZpY2F0ZWF1dGhvcml0eS9wdWJsaWMwCwYJYIZIAYb9bAIBMBMGA1UdJQQMMAoG
             CCsGAQUFBwMBMDUGA1UdHwQuMCwwKqAooCaGJGh0dHA6Ly9jcmwuYXBwbGUuY29t
-            L2FwZXZzcnNhMmcxLmNybDAdBgNVHQ4EFgQUkR204uwmGTkLM8SWtwcpGJfV3asw
-            DgYDVR0PAQH/BAQDAgWgMA8GCSqGSIb3Y2QGVgQCBQAwggH4BgorBgEEAdZ5AgQC
-            BIIB6ASCAeQB4gB3AD8XS0/XIkdYlB1lHIS+DRLtkDd/H4Vq68G/KIXs+GRuAAAB
-            kDcZ9Q4AAAQDAEgwRgIhAOjsftekDAX9X2/3excjhAD/kJtsZLAyCHkjAUi3R7cg
-            AiEAjNRxRI22qkHp8J/KUoxZiJgmdscawybTz9PW4ikpj9MAdwBIsONr2qZHNA/l
-            agL6nTDrHFIBy1bdLIHZu7+rOdiEcwAAAZA3GfT8AAAEAwBIMEYCIQC+cIxOI9IN
-            z3E6sUPoeFoUZXpWn+TO7YFivBI7wGix2gIhAJ2kPioANfCCjhYDMZ9+//6qdM18
-            6/wmzHi2gqFBkoBHAHYA2ra/az+1tiKfm8K7XGvocJFxbLtRhIU0vaQ9MEjX+6sA
-            AAGQNxn1LAAABAMARzBFAiAW/yq/wdU/xrjGGrexlMCamN6OGfWR1WGgHkkeD0z3
-            7gIhAOOb4fm1m6dZmDyqVdG38aJrW4zm2QNsD3Ze7Uhg7BMEAHYA7s3QZNXbGs7F
-            XLedtM0TojKHRny87N7DUUhZRnEftZsAAAGQNxn1AwAABAMARzBFAiEAviamxheQ
-            he3ZCQ/LC1M/kfUUfyNR5MVPDNmQtB3xTLcCIGBdVHVIlmkzaiv441tLm5UnCAE4
-            6jyAi/ZLQbfhmMPZMA0GCSqGSIb3DQEBCwUAA4IBAQC9UUM9oP39H2TcLdAVOAiY
-            VyM+MOENKn2p6Akaf3D/rZqrL9AVwQEA5SsRYwALmslOXJsMXdflQLL60jnWyNaq
-            WJ0UuOHAt3aB76q67Jsk+Kk3dNg4HPbu7PMvO1fBrjqd9ZIKG3m7rChoTDcean72
-            4Vg+yQ2mrSRBfVdt2QK61zOyxuEYknSTLzOq/WK8xOKLYgIahqKtlh7Xr+xPeQJN
-            PnhYLg5svRRpeLSnagx4eiPcnweHESkTmV3CjpJfKzAdt0gF1s8TzPpAnKmtuwcM
-            98vR8IkBkBkKzkuwkC2QJcioryRWALwErYRfwWZd1RU0Du3JwSJk9Cbi+rH3UclP
+            L2FwZXZzcnNhMmcxLmNybDAdBgNVHQ4EFgQUnYQjfWyYxNohSZQiCR7oNyhsduEw
+            DgYDVR0PAQH/BAQDAgWgMA8GCSqGSIb3Y2QGVgQCBQAwggH2BgorBgEEAdZ5AgQC
+            BIIB5gSCAeIB4AB2AD8XS0/XIkdYlB1lHIS+DRLtkDd/H4Vq68G/KIXs+GRuAAAB
+            kYBTDJsAAAQDAEcwRQIhAPYW1NYvfxVjbeLuVetuXlyZLDNKNNOPk/DIDP/ulW+s
+            AiBjNexLcU6HvsVJSsk9SXpuCgMeKE942dmBUsmNH1PgCwB3ANq2v2s/tbYin5vC
+            u1xr6HCRcWy7UYSFNL2kPTBI1/urAAABkYBTDN4AAAQDAEgwRgIhAN8y6nBmFFjB
+            6Wr3QZErziopyOpqztPnegAgXGLKWgzyAiEA53yPV9KHRJRemZ1btzY7+Ds7tUF3
+            e3s52DGzGTEgiVQAdgDuzdBk1dsazsVct520zROiModGfLzs3sNRSFlGcR+1mwAA
+            AZGAUwzFAAAEAwBHMEUCIByK/3OO2g8iokkvPO2uTKZukPRRhb9ZPwu4rd2oO9Sm
+            AiEAr9R7HVz3UBZuVwjcHv9LKWqoQ5Z0l0Pb5YyPZ17h9kcAdQAZmBBxCfDWUi4w
+            gNKeP2S7g24ozPkPUo7u385KPxa0ygAAAZGAUw0EAAAEAwBGMEQCICJs0tMQ5cUk
+            FXHGCIpX33RL9JqehQT34oqiSpSIBLSOAiAyWdjUUleeq7yqRElesURod3KSwPM+
+            +97hFsjxFaZLQjANBgkqhkiG9w0BAQsFAAOCAQEATLPqosjajvXRJ4V9fwb1XMDS
+            X4gCGKqdcso2q7C8kDTHUCduu4B0S/phIbJE+TrtjHaNwGO8oZG2rt9u7XQvnisr
+            ZwR5nAZMaW7MggsugWkhxl+DThbSJXZSwd44gn9+HjrNJkqdn+R07qxGdqe4io2b
+            eWY1KfY8ZFFkhSVtaQvEb0LjHh/YSi0UzUuaKIHyuwzQKE/3OFZkSsVaA4hxuJ0o
+            MPPVehmcin4dY7PEtOZYTcUmOfW3Nv+cdgjj84tH5urSbqN0CdaOG4xdCDqMhFgX
+            aIL2NGfQMTmPV6jCeOpm/8B/XoaD/rufYyfyI9H79vSwQQz9qoSy9D5dzNXvsA==
             """
 
         let intermediate = """
