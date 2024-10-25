6d99458e7f33a5addc5784548c85a3febc197ac6
Rick Newton-Rogers <rnro@apple.com>
2024-10-25 14:25:46 +0100

    Switch allocation benchmarks to GHA
    Motivation:
    
    This will allow us to decommission the old CI hooks.
    
    Modifications:
    
    * Migrate the existing tests to use the package-benchmark framework.
    * Delete the obsolete integration tests
    * Remove allocations test references from the docker files.
    
    Result:
    
    Allocation counts are checked using GitHub Actions.

---
+++ Benchmarks/Benchmarks/NIOSSLBenchmarks/Benchmarks.swift
@@ -0,0 +1,54 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Benchmark
+import NIOCore
+import NIOEmbedded
+import NIOSSL
+
+let benchmarks = {
+    let defaultMetrics: [BenchmarkMetric] = [
+        .mallocCountTotal
+    ]
+
+    Benchmark(
+        "SimpleHandshake",
+        configuration: .init(
+            metrics: defaultMetrics,
+            scalingFactor: .kilo,
+            maxDuration: .seconds(10_000_000),
+            maxIterations: 10,
+            thresholds: [.mallocCountTotal: .init(absolute: [.p90: 2000])]
+        )
+    ) { benchmark in
+        try runSimpleHandshake(
+            handshakeCount: benchmark.scaledIterations.upperBound
+        )
+    }
+
+    Benchmark(
+        "ManyWrites",
+        configuration: .init(
+            metrics: defaultMetrics,
+            scalingFactor: .kilo,
+            maxDuration: .seconds(10_000_000),
+            maxIterations: 10,
+            thresholds: [.mallocCountTotal: .init(absolute: [.p90: 2000])]
+        )
+    ) { benchmark in
+        try runManyWrites(
+            writeCount: benchmark.scaledIterations.upperBound
+        )
+    }
+}
---
+++ Benchmarks/Benchmarks/NIOSSLBenchmarks/SimpleHandshake.swift
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2019-2021 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIOCore
+import NIOEmbedded
+import NIOSSL
+
+func runSimpleHandshake(handshakeCount: Int) throws {
+    let serverContext = try NIOSSLContext(
+        configuration: .makeServerConfiguration(
+            certificateChain: [.certificate(.forTesting())],
+            privateKey: .privateKey(.forTesting())
+        )
+    )
+
+    var clientConfig = TLSConfiguration.makeClientConfiguration()
+    clientConfig.trustRoots = try .certificates([.forTesting()])
+    let clientContext = try NIOSSLContext(configuration: clientConfig)
+
+    let dummyAddress = try SocketAddress(ipAddress: "1.2.3.4", port: 5678)
+
+    for _ in 0..<handshakeCount {
+        let backToBack = BackToBackEmbeddedChannel()
+        let serverHandler = NIOSSLServerHandler(context: serverContext)
+        let clientHandler = try NIOSSLClientHandler(context: clientContext, serverHostname: "localhost")
+        try backToBack.client.pipeline.addHandler(clientHandler).wait()
+        try backToBack.server.pipeline.addHandler(serverHandler).wait()
+
+        // To trigger activation of both channels we use connect().
+        try backToBack.client.connect(to: dummyAddress).wait()
+        try backToBack.server.connect(to: dummyAddress).wait()
+
+        try backToBack.interactInMemory()
+
+        // Ok, now do shutdown.
+        backToBack.client.close(promise: nil)
+        try backToBack.interactInMemory()
+        try backToBack.client.closeFuture.wait()
+        try backToBack.server.closeFuture.wait()
+    }
+}
