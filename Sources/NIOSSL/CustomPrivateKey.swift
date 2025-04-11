f6a4469c1b222d816b74591952d4a003d7dcd70c
Cory Benfield <lukasa@apple.com>
2025-04-11 09:52:42 +0100

    Clean up sendability of statics (#523)
    A number of statics in the product code aren't something the compiler
    currently believes are Sendable. There are a few tweaks here:
    
    1. Two pointer statics into BoringSSL. These are thread-safe, but the
    compiler doesn't know. Mark them `nonisolated(unsafe)`.
    2. One static `var` should be a static `let`.
    3. One instance of reminding the compiler that closures that don't
    capture anything are Sendable.

--- Sources/NIOSSL/CustomPrivateKey.swift
+++ Sources/NIOSSL/CustomPrivateKey.swift
@@ -217,11 +217,18 @@ extension SSLConnection {
 
 // We heap-allocate the SSL_PRIVATE_KEY_METHOD we need because we can't define a static stored property with fixed address
 // in Swift.
-internal let customPrivateKeyMethod: UnsafePointer<SSL_PRIVATE_KEY_METHOD> = {
+#if compiler(>=5.10)
+nonisolated(unsafe) internal let customPrivateKeyMethod: UnsafePointer<SSL_PRIVATE_KEY_METHOD> =
+    buildCustomPrivateKeyMethod()
+#else
+internal let customPrivateKeyMethod: UnsafePointer<SSL_PRIVATE_KEY_METHOD> = buildCustomPrivateKeyMethod()
+#endif
+
+private func buildCustomPrivateKeyMethod() -> UnsafePointer<SSL_PRIVATE_KEY_METHOD> {
     let pointer = UnsafeMutablePointer<SSL_PRIVATE_KEY_METHOD>.allocate(capacity: 1)
     pointer.pointee = .init(sign: customKeySign, decrypt: customKeyDecrypt, complete: customKeyComplete)
     return UnsafePointer(pointer)
-}()
+}
 
 /// This is our entry point from BoringSSL when we've been asked to do a sign.
 private func customKeySign(
